9c2117dbbdd47ecd4b1d9261738a9e42eceeac53
alice clavel <aliceclavel@wanadoo.fr>
2015-02-16 19:09:23 +0100

    starting pet controller and pet index view

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,17 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+  end
+
+  def creaate
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pets/index'
+
+  get 'pets/show'
+
+  get 'pets/creaate'
+
+  get 'pets/update'
+
+  get 'pets/delete'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
