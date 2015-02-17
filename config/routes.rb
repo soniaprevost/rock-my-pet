f823d9ea513e666868b25483ad718d1cc7cb1436
alice clavel <aliceclavel@wanadoo.fr>
2015-02-17 17:15:57 +0100

    navbar and footer got improved

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root to: "home#index"
 
+  get 'about' => 'home#about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
