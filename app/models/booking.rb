8adad91c03122214ebddce86f1e4d3d523331a2b
alice clavel <aliceclavel@wanadoo.fr>
2015-02-19 16:07:49 +0100

    djhfshdqjhdfjhdfhjkqdjshkqjdhksjhkdqsjhkdhkqsjkhhdjkshjksdhkjdfhjkfsdhjkfdsqhkjshkshkjfqd

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,4 @@
 class Booking < ActiveRecord::Base
   belongs_to :pet
   belongs_to :walker, class_name: "User", foreign_key: :user_id
-  after_update :update_pet_status
-
-  private
-  def update_pet_status
-    self.pet.update_attributes(available: false) if self.accept
-  end
 end
